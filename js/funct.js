7fea0a3859bd71bf67cfb72ca7de502c5892bf9c
Kyle Lohrmann <123435933+lohrmannkyle@users.noreply.github.com>
2023-04-25 21:46:06 +0000

    Replace js genButtons with hard-coded html

--- js/funct.js
+++ js/funct.js
@@ -10,20 +10,3 @@ function add (num1, num2){
 function subtract (num1, num2){
     return num1 - num2;
 }
-function genButtons () {
-    const container = document.querySelector('.buttons');
-    for (let i = 3; i >= 1; i--){
-        const row = document.createElement('div');
-        row.classList.add('row');
-
-        for (let j = 3; j >= 1; j--) {
-            const button = document.createElement('button');
-            button.classList.add('button');
-            button.textContent = j*i;
-            row.appendChild(button);
-        }
-        container.appendChild(row);
-    }
-}
-
-genButtons()
