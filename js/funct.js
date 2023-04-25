53547747befa6ec21ec94f54ee7ce29aff142e8b
Kyle Lohrmann <123435933+lohrmannkyle@users.noreply.github.com>
2023-04-25 17:50:57 +0000

    Add button class

--- js/funct.js
+++ js/funct.js
@@ -10,3 +10,17 @@ function add (num1, num2){
 function subtract (num1, num2){
     return num1 - num2;
 }
+function genButtons () {
+    const container = document.querySelector('.buttons');
+    for (let i = 0; i < 3; i++){
+        const row = document.createElement('div');
+        row.classList.add('row');
+
+        for (let j = 0; j < 3; j++) {
+            const button = document.createElement('div');
+            button.classList.add('button-outline');
+            row.appendChild(button);
+        }
+        container.appendChild(row);
+    }
+}
