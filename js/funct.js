800ec82f0a3b3dcb5efeebb6081223185c18798c
Kyle Lohrmann <123435933+lohrmannkyle@users.noreply.github.com>
2023-04-25 18:32:01 +0000

    Update for loops in genButtons

--- js/funct.js
+++ js/funct.js
@@ -12,11 +12,11 @@ function subtract (num1, num2){
 }
 function genButtons () {
     const container = document.querySelector('.buttons');
-    for (let i = 0; i < 3; i++){
+    for (let i = 3; i <= 1; i--){
         const row = document.createElement('div');
         row.classList.add('row');
 
-        for (let j = 1; j <= 3; j++) {
+        for (let j = 3; j >= 1; j--) {
             const button = document.createElement('button');
             button.classList.add('button');
             button.textContent = j*i;
