bea5fc1451ee27070d7a1b32d7c95f031f67d313
Kyle Lohrmann <123435933+lohrmannkyle@users.noreply.github.com>
2023-04-26 22:02:21 +0000

    Add functions in js

--- js/funct.js
+++ js/funct.js
@@ -11,8 +11,28 @@ function subtract (num1, num2){
     return num1 - num2;
 }
 
-const numbers = document.querySelectorAll('#number');
-const display = document.querySelector('.display');
-numbers.forEach((number) => button.addEventListener('click', () => {
-    display.textContent += number.textContent;
-}))
+function hideFunctionButtons(){
+    const functions = document.querySelectorAll('#function');
+    functions.forEach((functionButt) => {
+        functionButt.classList.add('hidden');
+    })
+}
+let operator = ''
+let operand = ''
+function addButtonListeners () {
+    const display = document.querySelector('.display');
+    const characters = document.querySelectorAll('#character');
+    characters.forEach((character) => character.addEventListener('click', () => {
+        display.textContent += character.textContent;
+    }))
+
+    const functions = document.querySelectorAll('#function');
+    functions.forEach(functionButt => functionButt.addEventListener('click', () => {
+        if (display.textContent.length > 0){
+            operand = functionButt.textContent;
+            oprtator = display.textContent;
+            display.textContent = '';
+            }
+        }
+    ))
+}
