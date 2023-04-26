e8a1c276bbd43b4247ce305f30d401c8f2b55688
Kyle Lohrmann <123435933+lohrmannkyle@users.noreply.github.com>
2023-04-26 19:39:11 +0000

    Add js for button numbers

--- js/funct.js
+++ js/funct.js
@@ -10,3 +10,9 @@ function add (num1, num2){
 function subtract (num1, num2){
     return num1 - num2;
 }
+
+const buttons = document.querySelectorAll('.button');
+const display = document.querySelector('.display');
+buttons.forEach((button) => button.addEventListener('click', () => {
+    display.textContent += button.textContent;
+}))
